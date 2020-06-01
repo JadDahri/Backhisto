02259e84b819cd439e0e02cc4c279f7ca33ddee6
JadDahri <64443286+JadDahri@users.noreply.github.com>
2020-06-01 14:31:24 +0000

    Add files via upload

---
+++ blad-e/src/main/java/com/example/blade/BladEApplication.java
@@ -0,0 +1,13 @@
+package com.example.blade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BladEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BladEApplication.class, args);
+		
+	}
+
+}
---
+++ blad-e/src/test/java/com/example/blade/BladEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BladEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
